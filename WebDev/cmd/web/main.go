55384ecb3dd45c9e9bfcb0d327d6db110f9b4907
Anwar Mirzadeh <117970176+AnwarSaginbai@users.noreply.github.com>
2023-02-19 15:30:43 +0600

    Add files via upload

---
+++ WebDev/cmd/web/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"WebDev/internal/models"
+	"flag"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippets *models.SnippetModel
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
+	}
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
---
+++ WebDev/go.mod
@@ -0,0 +1,4 @@
+module WebDev
+
+go 1.19
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
